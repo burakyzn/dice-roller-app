9c0cf8b30a205b4a4d589aba149257825746b85e
Burak Yazan <44683436+burakyzn@users.noreply.github.com>
2019-06-11 11:35:44 +0300

    bug fixed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const select_box = document.querySelector('.form-control');
 const modal_body = document.querySelector('.modal-body');
 const container = document.querySelector('.container-fluid');
+const button = document.querySelector('.btn');
 var lastSelectedID = '';
 var selectedID = '';
 var result = '';
@@ -32,7 +33,6 @@ container.addEventListener('click',function(event){
 
     if(event.target.type == 'button'){
         $('.modal').modal('show');
-
         if(selectedID != ''){
             result = '';
             total_result = 0;
@@ -45,13 +45,18 @@ container.addEventListener('click',function(event){
 
             average = total_result / select_box.value;
 
+           
             setTimeout(function () {
                 modal_body.innerHTML = '<div class="result-numbers">' + result + '</div><div class="total-result">' + total_result + '</div><div class="result-numbers">' + average + '</div>';
             }, 1000);
             
         } else {
-            modal_body.innerHTML = '<div class="result-numbers"> Please select the dice </div>'
+            modal_body.innerHTML = '<div class="result-numbers"> Please select the dice </div>';
         }
     }
+})
+
+button.addEventListener('click',function(){
+    modal_body.innerHTML = '<div class="modal-cont"><i class="fas fa-dice-six rotate-dice-center"></i></div>'
 })
 
